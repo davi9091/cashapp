4abddee44949b3b057a60aba8ba36a4e2ba4c7e9
Arkady Rubtsov <arkadyrubtsov@gmail.com>
2021-04-03 16:22:48 +0300

    added cors

--- index.ts
+++ index.ts
@@ -1,15 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import {noteRouter} from "./routes";
 import {MongoClient} from "mongodb";
 import bodyParser from "body-parser";
-import {dbConfig} from "./config/db";
 import {DatabaseSingleton} from "./helpers/DatabaseSingleton";
+import {dbConfig} from "./config/db";
 
 const mongoClient = MongoClient;
 const app = express();
 const PORT = 3200;
 
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors());
 mongoClient.connect(dbConfig.url, (err, database) => {
     if (err) return console.log(err);
 
